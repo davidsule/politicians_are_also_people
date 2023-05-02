4828dd32d2d20261feeab73d1f57f40231c9946b
David Sule <david.sule@gmail.com>
2023-05-03 00:35:45 +0200

    Done rewrite of entity categorization

--- entity_categorization/dist_util.py
+++ entity_categorization/dist_util.py
@@ -67,7 +67,51 @@ def categories_from_pred(labels, entities):
             categories[label].append(entity)
     return categories
 
-def name_categories(categories, model, kth_closest=6):
+def name_categories(categories, model, stop_chars="._@", kth_closest=1):
+    """Get category dictionary out with the keys replaced with a word
+    close to the mean of the embeddings of the words for that label in
+    the embedding space.  Words containig any of `stop_chars` are
+    skipped and the next most similar takes their place.
+
+    Parameters:
+        categories (dict):  Output of `get_categories` function,
+            dictionary with labels as keys and list of corresponding
+            words as values.
+        model (gensim.models.keyedvectors.KeyedVectors):  The model
+            containing the embeddings.
+        stop_chars (str|list, optional):  Chars that category names
+            cannot contain. Defaults to '._@'.
+        kth_closest (int, optional):  Determines which word is selected
+            from the list of most similar ones.  Must be in a positive
+            int but less than the number of words in the model.
+            Defaults to 1 (the most similar one).
+    
+    Returns:
+        named_categories (dict):  Category dictionary with named labels.
+    """
+    named_categories = {}
+    for words in categories.values():
+        category = model.most_similar(positive=words, topn=kth_closest)[kth_closest-1][0]
+
+        # Handle the (extremely unlikely) case when two categories got the same name
+        # And when the name has any of the stop_chars in it
+        has_stop_char = False
+        for char in stop_chars:
+            if char in category:
+                has_stop_char = True
+        tmp = kth_closest + 1
+        while category in named_categories or has_stop_char:
+            category = model.most_similar(positive=words, topn=tmp)[tmp-1][0]
+            has_stop_char = False
+            for char in stop_chars:
+                if char in category:
+                    has_stop_char = True
+            tmp += 1
+
+        named_categories[category] = words
+    return named_categories
+
+def name_categories_old(categories, model, kth_closest=6):
     """Get category dictionary out with the keys replaced with a word
     close to the mean of the embeddings of the words for that label in
     the embedding space.  The `kth_closest` word is selected to be able
@@ -81,10 +125,10 @@ def name_categories(categories, model, kth_closest=6):
             words as values.
         model (gensim.models.keyedvectors.KeyedVectors):  The model
             containing the embeddings.
-        kth_closest (int):  Determines which word is selected from the
-            list of 10 most similar words.  Must be in a positive int
-            but less than the number of words in the model. Defaults to
-            6 (the 6th most similar).
+        kth_closest (int, optional):  Determines which word is selected
+            from the list of most similar ones.  Must be in a positive
+            int but less than the number of words in the model.
+            Defaults to 6 (the 6th most similar).
     
     Returns:
         named_categories (dict):  Category dictionary with named labels.
@@ -99,7 +143,6 @@ def name_categories(categories, model, kth_closest=6):
             tmp += 1
 
         named_categories[category] = words
-    
     return named_categories
 
 def most_frequent_synset(entities: list) -> list:
@@ -122,7 +165,7 @@ def minpath(synset) -> list:
             minlen_idx = idx
     return hypernym_paths[minlen_idx]
 
-def graph_category_dict(synset_list: list, level: int = 2, words: Union[list, None] = None) -> dict:
+def topological_category_dict(synset_list: list, level: int = 2, words: Union[list, None] = None) -> dict:
     """Create dictionary with desired level of hypernyms as keys and
     synsets as values.  Optionally pass the corresponding list of words
     to return the words as values.
@@ -136,8 +179,16 @@ def graph_category_dict(synset_list: list, level: int = 2, words: Union[list, No
     Each hypernym category is a key in the returned dictionary, whose
     keys are the synsets belonging to it from synset_list (or the
     corresponding words if words in not None).
+
+    Hypernym categories have the tailing numbering removed (e.g.
+    object.n.01 -> object).  If there are several different ones whose
+    short form would be the same, the full name is kept for the second
+    and further categories.
     """
     categories = {}
+    long_forms = set()
+    conflicts = set()
+    # Get category name
     for i, syn in enumerate(synset_list):
         path = minpath(syn)
         try:
@@ -145,16 +196,31 @@ def graph_category_dict(synset_list: list, level: int = 2, words: Union[list, No
         except:
             cat = path[-1]
         cat = cat.name()
-        if cat in categories:
+        # Shorten category name (object.n.01 -> object)
+        catname_to_add = cat.split(".")[0]
+        # If we haven't seen the category name yet but it's short form is
+        # already in the dictionary -> conflict: two different categories have
+        # the same short form -> add the long name to the dict + remember the
+        # conflicting category
+        if cat not in long_forms and catname_to_add in categories:
+            catname_to_add = cat
+            conflicts.add(cat)
+        # If we already know it's a conflict -> add the long name to the dict
+        elif cat in conflicts:
+            catname_to_add = cat
+        # Remember long form
+        long_forms.add(cat)
+
+        if catname_to_add in categories:
             if words is not None:
-                categories[cat].append(words[i])
+                categories[catname_to_add].append(words[i])
             else:
-                categories[cat].append(syn)
+                categories[catname_to_add].append(syn)
         else:
             if words is not None:
-                categories[cat] = [words[i]]
+                categories[catname_to_add] = [words[i]]
             else:
-                categories[cat] = [syn]
+                categories[catname_to_add] = [syn]
     return categories
 
 def get_wup_sim(entity_syns):
@@ -171,3 +237,39 @@ def get_wup_sim(entity_syns):
             similarities[i, j] = sim
             similarities[j, i] = sim
     return similarities
+
+def entities_to_orig(categories: dict, substitutions: dict) -> dict:
+    """Change substituted entity names to original ones in the final
+    category dictionary.
+
+    Parameters:
+        categories (dict):  Entity cluster dictionary with category
+            names as keys and list of (substituted) entities as values.
+        substitutions (dict):  Dictionary with original entities as keys
+            and substitutions as values.  Only needs to contain the
+            modified entities.
+    
+    Returns:
+        category_dict (dict):  The same dict as the categories input but
+            with the original entity names.
+    """
+    # The parts with the back_substitutions is so complicated because it needs
+    # to handle the situation when two of the original entities map to the same
+    # new substitute entity.
+    category_dict = {}
+    back_substitutions = {}
+    for orig_entity, substitue_entity in substitutions.items():
+        if substitue_entity not in back_substitutions:
+            back_substitutions[substitue_entity] = [orig_entity]
+        else:
+            back_substitutions[substitue_entity].append(orig_entity)
+    for category, new_entity_list in categories.items():
+        orig_entity_list = []
+        for new_entity in new_entity_list:
+            if new_entity in back_substitutions:
+                for orig_entity in back_substitutions[new_entity]:
+                    orig_entity_list.append(orig_entity)
+            else:
+                orig_entity_list.append(new_entity)
+        category_dict[category] = orig_entity_list
+    return category_dict
