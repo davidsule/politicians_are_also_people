51d5bb2df7e7e5be4cd2cce6392263f11bde5fe1
nicra <nicra@itu.dk>
2023-05-22 16:00:41 +0200

    run.sh

--- README.md
+++ README.md
@@ -1,38 +1,29 @@
 # 2yp_project
 Second Year Project repo
 
+## Research Question 
+Can we group entities in a meaningful way in cross-domain setups and inject these groupings to improve performance?
+
 ## File Structure
-- TBA
-- CrossRE
-    - Contains the CrossRE dataset and project scripts for reference.
-- project_rob
-    - Contains project description and requirements.
+- data
+    - crossre_data
+        - The training-, developing- and test-data
+    - predictions
+        - names of folders: DOMAIN-LIST_SEED where the domain list is abbreviated from the first letter in the domain 
+    - results 
+        - The results of the predictions
+- figures
+    - images of plotting.
+- src
+    - with all the script files
+- util
+    - helper functions to check results
+
+
+## How to run the training
 
 ## To-dos
 - Add requirements.txt
 - Add licence
 - Discuss eval of OOD clustering: diff nr of unique entitites + no unique to news
 - Rewrite README for reader of the project
-
-## Project Work Schedule
-- Create Groups / Hierarchy (Manual task)
-- Create Groups / Hierarchy (Automatically / wordnet)
-- Properly define baseline (train on 5, validate on last?)
-- Determine test paramaters
-- Execute testing
-- Create visuals / results for report
-- Write Report
-
-## Brain Storm Questions
- - which models are suited for cross-domain? 
- - What can be Improved in current methods?
- - How to make a baseline model? (Trained seperately per domain?)
- - How to handle labels in cross-domain?
- - What are we actually even trying to classify?
-
-### RC Project Proposal 3
-Can we SOMEHOW find a way to represent different entity types in different domains (i.e. use entity types from Reuters / Literature in the Music Domain) in the same subset of labels? 
-
-How do we Build Hierarchy of labels, that can represent entities and their relations across domains? 
-
-We want to PREDICT the label from a *specific subset* by some method, that still maintains the structure and meaning of extracted relations. That still remains well-performing, and thus is generalizable.
--- run.sh
+++ run.sh
@@ -1,161 +1,34 @@
 #!/bin/bash
+DATA_PATH=data/crossre_data/
+EXP_PATH=data/predictions
+OUT_PATH=data/results
 
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 4012 -shuffle -map None
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 4012 -shuffle -map manual
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 4012 -shuffle -map elisa
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 4012 -shuffle -map embedding
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 4012 -shuffle -map topological
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 4012 -shuffle -map thesaurus_affinity
+SEEDS=( 4012 5096 8878 8857 9908 )
+MAPPING=( "None" "manual" "embedding" "ood_clustering" "topological" "thesaurus_affinity" )
+for rs in "${!SEEDS[@]}"; do
+        for mapping in "${!MAPPING[@]}"; do
 
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 4012 -shuffle -ood_val -map None
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 4012 -shuffle -ood_val -map manual
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 4012 -shuffle -ood_val -map elisa
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 4012 -shuffle -ood_val -map embedding
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 4012 -shuffle -ood_val -map ood_clustering
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 4012 -shuffle -ood_val -map topological
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 4012 -shuffle -ood_val -map thesaurus_affinity
+            # No all for ood_clustering method
+            if [ ${MAPPING[$mapping]} != "ood_clustering" ] 
+            then
+                # training
+                python main.py --exp_path ${EXP_PATH} --data_path ${DATA_PATH} -rs ${SEEDS[$rs]} -shuffle -map ${MAPPING[$mapping]}
+                
+                # prediction
+                python main.py --exp_path ${EXP_PATH} --data_path ${DATA_PATH} -rs ${SEEDS[$rs]} -shuffle -po -map ${MAPPING[$mapping]}
+            fi
+            
+            # training
+            python main.py --exp_path ${EXP_PATH} --data_path ${DATA_PATH} -rs ${SEEDS[$rs]} -shuffle -ood_val -map ${MAPPING[$mapping]}
+            
+            # prediction
+            python main.py --exp_path ${EXP_PATH} --data_path ${DATA_PATH} -rs ${SEEDS[$rs]} -shuffle -po -ood_val -map ${MAPPING[$mapping]}
 
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 4012 -shuffle -po -map None
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 4012 -shuffle -po -map manual
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 4012 -shuffle -po -map elisa
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 4012 -shuffle -po -map embedding
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 4012 -shuffle -po -map topological
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 4012 -shuffle -po -map thesaurus_affinity
+    done
+done
 
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 4012 -shuffle -po -ood_val -map None
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 4012 -shuffle -po -ood_val -map manual
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 4012 -shuffle -po -ood_val -map elisa
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 4012 -shuffle -po -ood_val -map embedding
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 4012 -shuffle -po -ood_val -map ood_clustering
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 4012 -shuffle -po -ood_val -map topological
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 4012 -shuffle -po -ood_val -map thesaurus_affinity
+DATA_PATH=data/crossre_data
 
-
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 5096 -shuffle -map None
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 5096 -shuffle -map manual
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 5096 -shuffle -map elisa
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 5096 -shuffle -map embedding
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 5096 -shuffle -map topological
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 5096 -shuffle -map thesaurus_affinity
-
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 5096 -shuffle -ood_val -map None
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 5096 -shuffle -ood_val -map manual
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 5096 -shuffle -ood_val -map elisa
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 5096 -shuffle -ood_val -map embedding
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 5096 -shuffle -ood_val -map ood_clustering
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 5096 -shuffle -ood_val -map topological
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 5096 -shuffle -ood_val -map thesaurus_affinity
-
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 5096 -shuffle -po -map None
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 5096 -shuffle -po -map manual
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 5096 -shuffle -po -map elisa
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 5096 -shuffle -po -map embedding
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 5096 -shuffle -po -map topological
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 5096 -shuffle -po -map thesaurus_affinity
-
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 5096 -shuffle -po -ood_val -map None
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 5096 -shuffle -po -ood_val -map manual
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 5096 -shuffle -po -ood_val -map elisa
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 5096 -shuffle -po -ood_val -map embedding
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 5096 -shuffle -po -ood_val -map ood_clustering
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 5096 -shuffle -po -ood_val -map topological
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 5096 -shuffle -po -ood_val -map thesaurus_affinity
-
-
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8878 -shuffle -map None
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8878 -shuffle -map manual
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8878 -shuffle -map elisa
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8878 -shuffle -map embedding
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8878 -shuffle -map topological
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8878 -shuffle -map thesaurus_affinity
-
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8878 -shuffle -ood_val -map None
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8878 -shuffle -ood_val -map manual
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8878 -shuffle -ood_val -map elisa
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8878 -shuffle -ood_val -map embedding
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8878 -shuffle -ood_val -map ood_clustering
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8878 -shuffle -ood_val -map topological
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8878 -shuffle -ood_val -map thesaurus_affinity
-
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8878 -shuffle -po -map None
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8878 -shuffle -po -map manual
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8878 -shuffle -po -map elisa
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8878 -shuffle -po -map embedding
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8878 -shuffle -po -map topological
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8878 -shuffle -po -map thesaurus_affinity
-
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8878 -shuffle -po -ood_val -map None
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8878 -shuffle -po -ood_val -map manual
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8878 -shuffle -po -ood_val -map elisa
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8878 -shuffle -po -ood_val -map embedding
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8878 -shuffle -po -ood_val -map ood_clustering
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8878 -shuffle -po -ood_val -map topological
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8878 -shuffle -po -ood_val -map thesaurus_affinity
-
-
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8857 -shuffle -map None
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8857 -shuffle -map manual
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8857 -shuffle -map elisa
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8857 -shuffle -map embedding
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8857 -shuffle -map topological
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8857 -shuffle -map thesaurus_affinity
-
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8857 -shuffle -ood_val -map None
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8857 -shuffle -ood_val -map manual
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8857 -shuffle -ood_val -map elisa
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8857 -shuffle -ood_val -map embedding
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8857 -shuffle -ood_val -map ood_clustering
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8857 -shuffle -ood_val -map topological
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8857 -shuffle -ood_val -map thesaurus_affinity
-
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8857 -shuffle -po -map None
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8857 -shuffle -po -map manual
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8857 -shuffle -po -map elisa
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8857 -shuffle -po -map embedding
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8857 -shuffle -po -map topological
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8857 -shuffle -po -map thesaurus_affinity
-
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8857 -shuffle -po -ood_val -map None
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8857 -shuffle -po -ood_val -map manual
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8857 -shuffle -po -ood_val -map elisa
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8857 -shuffle -po -ood_val -map embedding
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8857 -shuffle -po -ood_val -map ood_clustering
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8857 -shuffle -po -ood_val -map topological
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 8857 -shuffle -po -ood_val -map thesaurus_affinity
-
-
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 9908 -shuffle -map None
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 9908 -shuffle -map manual
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 9908 -shuffle -map elisa
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 9908 -shuffle -map embedding
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 9908 -shuffle -map topological
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 9908 -shuffle -map thesaurus_affinity
-
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 9908 -shuffle -ood_val -map None
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 9908 -shuffle -ood_val -map manual
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 9908 -shuffle -ood_val -map elisa
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 9908 -shuffle -ood_val -map embedding
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 9908 -shuffle -ood_val -map ood_clustering
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 9908 -shuffle -ood_val -map topological
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 9908 -shuffle -ood_val -map thesaurus_affinity
-
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 9908 -shuffle -po -map None
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 9908 -shuffle -po -map manual
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 9908 -shuffle -po -map elisa
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 9908 -shuffle -po -map embedding
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 9908 -shuffle -po -map topological
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 9908 -shuffle -po -map thesaurus_affinity
-
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 9908 -shuffle -po -ood_val -map None
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 9908 -shuffle -po -ood_val -map manual
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 9908 -shuffle -po -ood_val -map elisa
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 9908 -shuffle -po -ood_val -map embedding
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 9908 -shuffle -po -ood_val -map ood_clustering
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 9908 -shuffle -po -ood_val -map topological
-python main.py --exp_path data/predictions --data_path data/crossre_data/ -rs 9908 -shuffle -po -ood_val -map thesaurus_affinity
-
-
-
-# Eval command
-ython evaluate.py --data_path data/crossre_data --exp_path data/predictions --out_path data/results -rs 4012 5096 8857 8878 9908 -map ood_clustering None manual embedding thesaurus_affinity topological
-ython evaluate.py --data_path data/crossre_data --exp_path data/predictions --out_path data/results -rs 4012 5096 8857 8878 9908 -map ood_clustering None manual embedding thesaurus_affinity topological -ood_val
+# evaluation
+python evaluate.py --data_path ${DATA_PATH} --exp_path ${EXP_PATH} --out_path ${OUT_PATH} -rs ${SEEDS[@]} -map ${MAPPING[@]}
+python evaluate.py --data_path ${DATA_PATH} --exp_path ${EXP_PATH} --out_path ${OUT_PATH} -rs ${SEEDS[@]} -map ${MAPPING[@]} -ood_val
